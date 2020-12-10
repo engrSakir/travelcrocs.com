ece18ab8f1d87f7048bfc83b6f56ad4e48462340
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2020-12-10 23:26:29 +0600

    Meta update
    if array value not exists default no ____

--- resources/views/layouts/administrative/app.blade.php
+++ resources/views/layouts/administrative/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8"/>
     <title>{{ config('app.name') }} | @stack('title')</title>
-    <meta name="author" content="{{ get_content_by_language('application_seo')['author'] }}">
-    <meta name="description" content="{{ get_content_by_language('application_seo')['description'] }}"/>
+    <meta name="author" content="{{ get_content_by_language('application_seo')['author'] ?? 'No author' }}">
+    <meta name="description" content="{{ get_content_by_language('application_seo')['description'] ?? 'No description' }}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <!--begin::CSRF-->
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/layouts/user/partials/head.blade.php
+++ resources/views/layouts/user/partials/head.blade.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 <title>{{ config('app.name') }} | @stack('title')</title>
-<meta name="author" content="{{ get_content_by_language('application_seo')['author'] }}">
-<meta name="description" content="{{ get_content_by_language('application_seo')['description'] }}"/>
+<meta name="author" content="{{ get_content_by_language('application_seo')['author'] ?? 'No author' }}">
+<meta name="description" content="{{ get_content_by_language('application_seo')['description'] ?? 'No description' }}"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <!-- Favicon -->
